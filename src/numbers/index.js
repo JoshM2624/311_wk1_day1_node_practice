9b3edd141c30052d7b364f25e8efd8e1ab6267b7
JoshM2624 <joshm2624@gmail.com>
2019-07-08 20:59:30 -0500

    first push

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,15 +1,27 @@
 const isEven = (num) => {
   // write code for numbers.isEven
+  if(num% 2 ===0){
+    return true
+
+  }
+  
 
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
+  return (arr.length === 0) ? 0 : arr[0] +sum(arr.slice(1));
+  //return 21
 
 }
 
-const comboSum = (arr, sum) => {
+const comboSum = (arr, addIt) => {
   // write code for numbers.comboSum
+  return false
+ let thisSum = sum(arr)
+ if(thisSum == addIt){
+   return true
+ }
 
 }
 
